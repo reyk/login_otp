67108e1a8a9175bf4ae2cab8c01db5df8011b5a4
Reyk Floeter <contact@reykfloeter.com>
2018-06-08 22:47:15 +0200

    Update copyright

--- oath.h
+++ oath.h
@@ -1,5 +1,26 @@
+/*
+ * Copyright (c) 2018 Reyk Floeter <contact@reykfloeter.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef _OATH_H
+#define _OATH_H
+
 int	 oath(unsigned char *, size_t keylen, uint64_t, uint8_t, uint16_t);
 int	 oath_totp(unsigned char *, size_t, time_t, time_t, uint8_t, uint16_t);
 int	 oath_hotp(unsigned char *, size_t, uint64_t, uint8_t);
 size_t	 oath_decode_key(char *, unsigned char *, size_t);
 int	 oath_generate_key(size_t);
+
+#endif /* _OATH_H */
