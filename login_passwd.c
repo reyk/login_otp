a3ded91c84a1ff20c1815f604fc2fc6526b8d60d
Reyk Floeter <contact@reykfloeter.com>
2018-06-10 01:34:30 +0200

    Don't call getpwnam_shadow(), we're under pledge

--- login_passwd.c
+++ login_passwd.c
@@ -30,9 +30,8 @@
 
 int
 pwd_login(char *username, char *password, char *wheel, int lastchance,
-    char *class)
+    char *class, struct passwd *pwd)
 {
-	struct passwd *pwd;
 	size_t plen;
 	char *goodhash = NULL;
 	int passok = 0;
@@ -46,7 +45,6 @@ pwd_login(char *username, char *password, char *wheel, int lastchance,
 	if (password == NULL)
 		return (AUTH_FAILED);
 
-	pwd = getpwnam_shadow(username);
 	if (pwd)
 		goodhash = pwd->pw_passwd;
 
